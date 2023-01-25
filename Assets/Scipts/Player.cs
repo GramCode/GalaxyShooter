b5887b898bffff4bed2e654980acc88e345aba03
Miguel Pacheco <miguel95_angel@hotmail.com>
2023-01-25 14:54:56 -0400

    Created a Laser game object, a prefab from the laser, instantiate the laser, give it behavior and destroy the laser.

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -6,12 +6,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
 
+
+    [SerializeField] private GameObject _laserPrefab;
+
     void Update()
+    {
+        CalculateMovement();
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        //Check if the space key is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    void CalculateMovement()
     {
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horizontalAxis * _speed * Time.deltaTime, verticalAxis * _speed * Time.deltaTime, 0));
     }
-
 }
