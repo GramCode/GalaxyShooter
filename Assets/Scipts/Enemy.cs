b54a878984dee883820a99cea2355867eb0bacd9
Miguel Pacheco <miguel95_angel@hotmail.com>
2023-01-27 12:28:50 -0400

    Adding Rigidbody and collisions

---
+++ Assets/Scipts/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyBehavior();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Laser" || other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    void EnemyBehavior()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -5.6f)
+        {
+            float randomX = Random.Range(-9, 9);
+            transform.position = new Vector3(randomX, 7.5f, 0);
+        }
+    }
+}
